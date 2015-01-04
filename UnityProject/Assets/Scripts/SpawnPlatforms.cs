d41798e8707a5d50d79559c3e5d828dade794304
Kevin Phung <phung.kevin@gmail.com>
2015-01-04 13:51:10 -0800

    new cloud assets; added bird assets; created new cloud group; updated spawn script to spawn groups instead of individual clouds;

--- UnityProject/Assets/Scripts/SpawnPlatforms.cs
+++ UnityProject/Assets/Scripts/SpawnPlatforms.cs
@@ -3,12 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class SpawnPlatforms : MonoBehaviour {
-	public float minY = -2.0f;
-	public float maxY = 2.0f;
+	public float minY = -15.0f;
+	public float maxY = 15.0f;
 	public List<GameObject> spawnList;
 	// Use this for initialization
 	void Start () {
-		InvokeRepeating("GeneratePlatform", 1.0f, 1.0f);
+		InvokeRepeating("GeneratePlatform", 0.30f, 2.0f);
 	}
 	
 	// Update is called once per frame
@@ -21,6 +21,7 @@ public class SpawnPlatforms : MonoBehaviour {
 		int rindex = Random.Range(0, spawnList.Count);
 		Vector3 pos = this.transform.position;
 		pos.y = pos.y + Random.Range (minY, maxY);
+		Debug.Log ("spawn cloud at y position = " + pos.y);
 		Quaternion rot = this.transform.rotation;
 		GameObject.Instantiate(spawnList[rindex], pos, rot);
 	}
