51612e619822bc3233f363c1a02f2539516ea75f
Calvin Phung <RynsArgent@hotmail.com>
2015-01-04 22:35:36 -0800

    Jump Strength

--- UnityProject/Assets/Scripts/TestCharController.cs
+++ UnityProject/Assets/Scripts/TestCharController.cs
@@ -1,34 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TestCharController : MonoBehaviour {
 
 	public float maxSpeed = 10f;
-	public float jumpForce = 300f;
 
-	bool grounded = false;
+	private bool jumping;
+	public List<float> jumpForces;
+	public float jumpMin = 0.25f;
+	public float jumpMax = 0.50f;
+	private float jumpTimer;
+
+	private bool grounded;
 	public Transform groundCheck;
-	float groundRadius = 0.1f;
+	private float groundRadius = 0.1f;
 	public LayerMask whatIsGround;
 	
 	// Use this for initialization
 	void Start () {
+		jumpTimer = jumpMax;
+		grounded = false;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (grounded && Input.GetKeyDown (KeyCode.Space)) {
-			rigidbody2D.AddForce(new Vector2(0, jumpForce));
+		if (grounded && !jumping && Input.GetKeyDown (KeyCode.Space))
+		{
+			StartCoroutine(performJump());
 		}
 	}
 
 	void FixedUpdate () {
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
 
-		float move = Input.GetAxis ("Horizontal");
-
-		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
 		Physics2D.IgnoreLayerCollision(gameObject.layer, LayerMask.NameToLayer("Platforms"), !grounded);
+	}
+
+	private IEnumerator performJump()
+	{
+		jumping = true;
+		int jumpPhase = 0;
+		for (; jumpPhase < jumpForces.Count && Input.GetKey (KeyCode.Space); ++jumpPhase)
+		{
+			rigidbody2D.AddForce(new Vector2(0.0f, jumpForces[jumpPhase]));
+			yield return new WaitForSeconds(0.1f);
+		}
+		jumping = false;
 	}
 }
