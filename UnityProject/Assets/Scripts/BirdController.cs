9579a90c6d7a62d141e8c5c6657daead5628a56e
Kevin Phung <phung.kevin@gmail.com>
2015-01-06 22:39:30 -0800

    new cloud formations; better bird speed and acceleration;

--- UnityProject/Assets/Scripts/BirdController.cs
+++ UnityProject/Assets/Scripts/BirdController.cs
@@ -5,13 +5,24 @@ public class BirdController : MonoBehaviour {
 
 	public float minSpeed;
 	public float maxSpeed;
+	public float initialSpeed;
+	public float speedVariance;
 
+	public float initialAcceleration;
+	public float maxAcceleration;
+	public float accelerationIncrement;
+
+	float acceleration;
 	float moveSpeed;	// determined by a random number between minSpeed and maxSpeed
 
 	// Use this for initialization
 	void Start () {
-		moveSpeed = Random.Range (minSpeed, maxSpeed);
+		//moveSpeed = Random.Range (minSpeed, maxSpeed);
+		moveSpeed = initialSpeed + Random.Range (0, speedVariance);
+		//moveSpeed = minSpeed;
+		acceleration = initialAcceleration;
 		rigidbody2D.velocity = new Vector2 (-1.0f * moveSpeed, rigidbody2D.velocity.y);
+
 		Physics2D.IgnoreLayerCollision (gameObject.layer, LayerMask.NameToLayer ("Platforms"), true);
 		Physics2D.IgnoreLayerCollision (gameObject.layer, LayerMask.NameToLayer ("Enemies"), true);
 	}
@@ -22,6 +33,11 @@ public class BirdController : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
-
+		acceleration = Mathf.Clamp (acceleration + accelerationIncrement, initialAcceleration, maxAcceleration);
+		//moveSpeed = Mathf.Clamp(moveSpeed + acceleration, minSpeed, maxSpeed);
+		//Debug.Log ("moveSpeed: " + moveSpeed + " _ DeltaTime: " + Time.deltaTime);
+		//rigidbody2D.velocity = new Vector2 (-1.0f * moveSpeed, rigidbody2D.velocity.y);
+		//rigidbody2D.AddForce(new Vector2(acceleration * rigidbody2D.mass, 0.0f)); 
+		rigidbody2D.AddRelativeForce (new Vector2 (acceleration * rigidbody2D.mass, 0.0f));
 	}
 }
