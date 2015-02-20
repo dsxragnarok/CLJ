2eba52281eac6e6b19339fe4ee5fc042194ab6e5
Kevin Phung <phung.kevin@gmail.com>
2015-02-19 20:24:21 -0800

    change instruction text for android

---
+++ UnityProject/Assets/Scripts/JumpInstruction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class JumpInstruction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+#if UNITY_ANDROID
+		GameObject spaceTextObj = GameObject.Find ("SpaceText");
+		Text spaceText = spaceTextObj.GetComponent<Text>();
+		Debug.Log (spaceText.text);
+		spaceText.text = "Tap to jump";
+#endif
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
