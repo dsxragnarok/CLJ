c4fc82f65f82cc86f83586a8b96b36ab52399dcc
Kevin Phung <phung.kevin@gmail.com>
2015-03-15 18:02:53 -0700

    Implemented check point system; added check point to each scene; tweaked star placements;

---
+++ UnityProject/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPoint : Entity {
+	Transform player;
+	Transform myTransform;
+
+	float moveSpeed = 4.5f;
+	bool collected = false;
+	public int scoreBonus = 200;
+
+	// Use this for initialization
+	public override void Start () {
+		base.Start ();
+		myTransform = this.transform;
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+		base.Update ();
+		if (gameMaster.Player.IsDead ())
+			moveSpeed = 0f;
+		
+		if (!collected && player.position.x >= myTransform.position.x) {
+			ActivateCheckPoint();
+		}
+	}
+
+	void FixedUpdate () {
+		if (gameMaster.isGameStarted) {
+			Vector3 pos = transform.position;
+			pos.x = pos.x - moveSpeed * Time.fixedDeltaTime;
+			transform.position = pos;
+			
+			if (gameMaster.GameBounds.IsOutOfBounds (this.gameObject)) {
+				GameObject.Destroy (this.gameObject);
+			}
+		}
+	}
+
+	void ActivateCheckPoint () {
+		collected = true;
+		gameMaster.updateScore (scoreBonus);
+		gameMaster.generateFloatingTextAt(gameMaster.Player.transform.position, "Check Point!\n"+scoreBonus.ToString());
+		GameObject.Destroy (this.gameObject, 2.0f);
+	}
+}
