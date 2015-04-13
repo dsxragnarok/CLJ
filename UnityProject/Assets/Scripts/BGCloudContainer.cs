169969d72352b9cbf13f96e5edc41821065d636a
Kevin Phung <phung.kevin@gmail.com>
2015-04-12 19:34:51 -0700

    Implement a slowly moving background clouds (paralax scrolling, self-recycle)

---
+++ UnityProject/Assets/Scripts/BGCloudContainer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+// The class containing a list of all the background cloud objects
+// The list keeps the clouds in order so that the background clouds
+// can reposition themselves based on position of the last cloud
+public class BGCloudContainer : Entity {
+
+    public List<GameObject> CloudBackgrounds;
+
+	// Use this for initialization
+	public override void Start () {
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+        base.Update();
+	}
+
+    void FixedUpdate()
+    {
+        foreach (GameObject cloud in CloudBackgrounds)
+        {
+            CloudBG cbg = cloud.GetComponent<CloudBG>();
+            cbg.Move(Time.fixedDeltaTime);
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/CloudBG.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+// Slowly scrolls the background cloud objects
+public class CloudBG : Entity {
+    private float moveSpeed = 1.0f;
+    private float width;
+
+    BGCloudContainer parentContainer;
+
+	// Use this for initialization
+	public override void Start () {
+        base.Start();
+        width = GetComponent<Renderer>().bounds.size.x;
+        parentContainer = GetComponentInParent<BGCloudContainer>();
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+        base.Update();
+        
+        if (transform.position.x + width <= gameMaster.GameBounds.Left)
+        {
+            GameObject lastCloud = parentContainer.CloudBackgrounds[parentContainer.CloudBackgrounds.Count - 1];
+            transform.position = new Vector3(lastCloud.transform.position.x + width, transform.position.y, transform.position.z);
+
+            parentContainer.CloudBackgrounds.Remove(this.gameObject);
+            parentContainer.CloudBackgrounds.Add(this.gameObject);
+        }
+         
+	}
+
+    public void Move (float deltaTime)
+    {
+        if (gameMaster.isGameStarted)
+        {
+            Vector3 pos = transform.position;
+            pos.x = pos.x - moveSpeed * deltaTime;
+            transform.position = pos;
+        }
+    }
+}
